dd8edd4caba0dbc97b750235326071f824b75d42
kkindorf <kevin.kindorf@gmail.com>
2018-05-17 19:31:21 -0400

    updating package.json

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,4 +1,5 @@
 export const CURRENT_STOCK_DATA = 'current_stock_data';
 export const NEW_STOCK_DATA = 'new_stock_data';
 export const HANDLE_SUBMISSION_ERROR = 'handle_submission_error';
+export const USER_SUBMISSION_SUCCESS = 'user_submission_success';
 export const REMOVE_STOCK = 'remove_stock';
